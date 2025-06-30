a5aa772ae4945291a292aba4d91de8510c5e021f
Oscar Rojas <ozkrrr9@gmail.com>
2025-06-30 08:51:37 -0400

    Refine credits section styling in Streamlit app
    - Adjusted the styling of the credits section by removing the background color for a cleaner look while maintaining the border and padding.
    - Ensured the section remains visually appealing and user-friendly for better engagement.

--- 03_Random_Forest/streamlit/creditos.py
+++ 03_Random_Forest/streamlit/creditos.py
@@ -15,7 +15,7 @@ def show_credits_section():
     # Bloque destacado para el repositorio
     st.markdown(
         """
-        <div style='background-color: #f0f2f6; padding: 20px; border-radius: 10px; border: 2px solid #24292f; text-align: center; margin-bottom: 20px;'>
+        <div style=' padding: 20px; border-radius: 10px; border: 2px solid #24292f; text-align: center; margin-bottom: 20px;'>
             <h3 style='margin-bottom: 10px;'>Repositorio del proyecto en GitHub</h3>
             <a href='https://github.com/IL3-DS-Trabajo-Final-Heart-Attack/IL3_DS_Trabajo_Final_Heart_Attack' target='_blank' style='display: inline-block; background-color: #24292f; color: #fff; padding: 12px 28px; border-radius: 6px; font-size: 18px; font-weight: bold; text-decoration: none; margin-top: 10px;'>
                 ⭐ Visítanos en GitHub
